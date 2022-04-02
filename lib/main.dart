b5610baf617ccc3097e8207130d5f479ad70c747
Harshita Raghav <raghav.harshita21@gmail.com>
2022-04-02 23:54:15 +0530

    This is my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,4 @@
-// import 'package:flutter/material.dart';
-// import './categories_screen.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'DeliMeals',
-//       theme: ThemeData(
-//         primarySwatch: Colors.pink,
-//         accentColor: Colors.amber,
-//         canvasColor: Color.fromRGBO(255, 254, 229, 1),
-//         fontFamily: 'Raleway',
-//         textTheme: ThemeData.light().textTheme.copyWith(
-//               bodyText1: TextStyle(color: Colors.black),
-//               bodyText2: TextStyle(color: Colors.brown),
-//               title: TextStyle(
-//                   fontSize: 20,
-//                   fontFamily: 'RobotCodense',
-//                   fontWeight: FontWeight.bold),
-//             ),
-//       ),
-//     );
-
-//     home:
-//     CategoriesScreen();
-//   }
-// }
-
 import 'package:flutter/material.dart';
-
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -46,19 +14,17 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(
-                color: Colors.black,
-              ),
-              bodyText2: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              // titleSmall: TextStyle(
-              //   fontSize: 20,
-              //   fontFamily: 'RobotoCondensed',
-              //   fontWeight: FontWeight.bold,
+              bodyText1: TextStyle(color: Colors.black),
+              bodyText2: TextStyle(color: Colors.brown),
+              titleMedium: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotCodense',
+                  fontWeight: FontWeight.bold),
             ),
       ),
-      home: CategoriesScreen(),
     );
+
+    home:
+    CategoriesScreen();
   }
 }
