906c9408b4861cf4f92aad1890e718caa5f3f3a6
Harshita Raghav <raghav.harshita21@gmail.com>
2022-04-02 19:32:32 +0530

    This is my first commit

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('The recipie for the category! '),
+      ),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'models/category.dart';
+
+var DUMMY_CATEGORIES = [
+  Category(id: 'c1', title: 'Indian', color: Colors.red),
+  Category(
+      id: 'c2', title: 'Italian', color: Color.fromARGB(255, 51, 126, 188)),
+  Category(
+      id: 'c3', title: 'Mexican', color: Color.fromARGB(255, 193, 123, 205)),
+  Category(id: 'c4', title: 'Chinese', color: Color.fromARGB(255, 255, 153, 0)),
+  Category(id: 'c5', title: 'Thai', color: Color.fromARGB(255, 136, 42, 35)),
+  Category(id: 'c6', title: 'Exotic', color: Colors.pink),
+  Category(id: 'c7', title: 'Burger and veggies', color: Colors.cyan),
+  Category(id: 'c1', title: 'French', color: Colors.lightBlue),
+  Category(id: 'c10', title: 'Asian', color: Colors.brown),
+  Category(id: 'c1', title: 'Breakfast', color: Colors.lime),
+  Category(id: 'c1', title: 'Lunch', color: Colors.amber),
+  Category(id: 'c1', title: 'Dinner', color: Color.fromARGB(255, 141, 193, 64)),
+];
+
+// import 'package:flutter/material.dart';
+
+// import './category.dart';
+
+// var DUMMY_CATEGORIES = [
+//   Category(
+//     id: 'c1',
+//     title: 'Italian',
+//     color: Colors.purple,
+//   ),
+//   Category(
+//     id: 'c2',
+//     title: 'Quick & Easy',
+//     color: Colors.red,
+//   ),
+//   Category(
+//     id: 'c3',
+//     title: 'Hamburgers',
+//     color: Colors.orange,
+//   ),
+//   Category(
+//     id: 'c4',
+//     title: 'German',
+//     color: Colors.amber,
+//   ),
+//   Category(
+//     id: 'c5',
+//     title: 'Light & Lovely',
+//     color: Colors.blue,
+//   ),
+//   Category(
+//     id: 'c6',
+//     title: 'Exotic',
+//     color: Colors.green,
+//   ),
+//   Category(
+//     id: 'c7',
+//     title: 'Breakfast',
+//     color: Colors.lightBlue,
+//   ),
+//   Category(
+//     id: 'c8',
+//     title: 'Asian',
+//     color: Colors.lightGreen,
+//   ),
+//   Category(
+//     id: 'c9',
+//     title: 'French',
+//     color: Colors.pink,
+//   ),
+//   Category(
+//     id: 'c10',
+//     title: 'Summer',
+//     color: Colors.teal,
+//   ),
+// ];
